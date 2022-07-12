e8de70e2f25ad6bf23209a3e29d18141002d6a53
Marcelo Olivas <mfolivas@gmail.com>
2022-07-12 15:39:56 -0400

    Internal - cleaning up

--- network/find-all-vpcs-in-all-regions.sh
+++ network/find-all-vpcs-in-all-regions.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+
 Profile='default'
+VPC_DETAILS_FILE='/tmp/vpc-details.csv'
 aws ec2 describe-regions \
+    --profile $Profile \
     --all-regions \
     --query "Regions[].{Name:RegionName}" \
-    --output text > regions.txt
-while read f1 f2
+    --output text > /tmp/regions.txt
+while read f1
       do 
       Region=$f1
-      Region_Name=$f2
-      echo "$Region - $Region_Name \n" >> /tmp/vpc-details.csv
-      aws ec2 describe-vpcs --profile $Profile --region $Region --output text --query 'Vpcs[].[Tags[?Key==`Name`].Value | [0],CidrBlock,VpcId,IsDefault]' --filter Name=isDefault,Values=false >> vpc-details.csv
-      echo "\n" >> vpc-details.csv  
-done < regions.txt
+      echo "$Region\n" >> $VPC_DETAILS_FILE
+      aws ec2 describe-vpcs --profile $Profile --region $Region --output text --query 'Vpcs[].[Tags[?Key==`Name`].Value | [0],CidrBlock,VpcId,IsDefault]' --filter Name=isDefault,Values=false >> $VPC_DETAILS_FILE
+      echo "\n" >> $VPC_DETAILS_FILE  
+done < /tmp/regions.txt``
